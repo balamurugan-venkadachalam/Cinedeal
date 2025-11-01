64ec62083022fcfdca1f1225a1106bda55a4987e
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2025-11-01 22:20:43 +1300

    refactor: update name

--- src/main/java/com/ticket/transaction/TicketTransactionApiApplication.java
+++ src/main/java/com/ticket/transaction/TicketTransactionApiApplication.java
@@ -1,13 +1,13 @@
-package com.cinedeals.booking;
+package com.ticket.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingApplication {
+public class TicketTransactionApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingApplication.class, args);
+		SpringApplication.run(TicketTransactionApiApplication.class, args);
 	}
 
 }
--- src/test/java/com/ticket/transaction/BookingApplicationTests.java
+++ src/test/java/com/ticket/transaction/BookingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cinedeals.booking;
+package com.ticket.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
