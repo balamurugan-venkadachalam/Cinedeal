434698a298c6878d86d0bd4bd1d3d51c44b8569c
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2025-11-03 13:38:27 +1300

    fix: number format

---
+++ src/main/java/com/ticket/transaction/config/BigDecimalSerializer.java
@@ -0,0 +1,23 @@
+package com.ticket.transaction.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class BigDecimalSerializer extends JsonSerializer<BigDecimal> {
+    
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (value == null) {
+            gen.writeNull();
+        } else {
+            gen.writeNumber(value.setScale(2, RoundingMode.HALF_UP));
+        }
+    }
+}
